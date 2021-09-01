c65d3da4df72f5f90f9f7f9f1ef65a9f9290cc55
Chappellm <Chappelm1@hawkmail.newpaltz.edu>
2021-09-01 16:50:43 -0400

    almost working?

--- NFA.py
+++ NFA.py
@@ -8,22 +8,23 @@ m.geometry('300x300')
 
 #global vars
 accepted_move = ""
-
+output_moves = []
 #funtions
-def inputfilewrite(eq,es,est,eff,moves):
+
+def inputfilewrite(states, letters,start,accept,moves):
     filewrite = open('input.txt', 'w')
-    filewrite.write(eq + '\n')
-    filewrite.write(es + '\n')
-    filewrite.write(est + '\n')
-    filewrite.write(eff + '\n')
-    filewrite.write(moves + '\n')
+    filewrite.write(str(states) + '\n')
+    filewrite.write(str(letters) + '\n')
+    filewrite.write(str(start) + '\n')
+    filewrite.write(str(accept) + '\n')
+    filewrite.write(str(moves) + '\n')
     filewrite.close()
 
-def acceptsA(i,m,states):
+def accepts(i,m,states,letter):
     for x in m:
         y = 1
         while True:
-            str = i + " a " + y
+            str = i + " " + letter + " " + y
             if str == m[x]:
                 accepted_move = str
                 return True
@@ -33,41 +34,34 @@ def acceptsA(i,m,states):
                 break
     return False
 
-def acceptsB(i,m,states):
-    for x in m:
-        y = 1
-        while True:
-            str = i + " b " + y
-            if str == m[x]:
-                accepted_move = str
-                return True
-            else:
-                y = y + 1
-            if y == len(states):
-                break
-    return False
+
 
 def run(start, states, moves, accept, letters):
     inputfilewrite(states, letters,start,accept,moves)
-    output_moves = []
+    
     #fileread = open('input.txt', 'r')
     x = datetime.datetime.now()
     outf = x.strftime('%H%d') + ".txt"
-    fileout = open(outf, 'x')
+    if (open(outf, 'r')):
+        fileout = open(outf, 'w')
+    else:
+        fileout = open(outf, 'x')
 
     i = start # starting state
-    move = moves.split(', ')
-    for i in states:
-        if (acceptsA(i,move,states)):
-            output_moves[i] = accepted_move
-        else:
-            output_moves[i] = i + ' a ' + i
-        if (acceptsB(i,move,states)):
-            output_moves[i] = accepted_move
-        else:
-            output_moves[i] = i + ' b ' + i
+    move = str(moves).split(', ')
+    for i in range(len(str(states))):
+        for let in range(len(str(letters))):
+            if (accepts(i,move,states,letters[let])):
+                output_moves[i] = accepted_move
+            else:
+                output_moves[i] = str(i) + " " + letters[let] + " " + str(i)
+    prntNFA()
 
-def prntmvs():
+def prntNFA():
+    for x in output_moves:
+        print(x)
+
+
     
 
     
@@ -83,30 +77,30 @@ def prntmvs():
 # widgets
 # input
 tkinter.Label(m, text='States:').grid(row=0, column=0)
-eq = tkinter.Entry(m)
-eq.grid(row=0, column=1, columnspan=3)
+states = tkinter.Entry(m)
+states.grid(row=0, column=1, columnspan=3)
 
 tkinter.Label(m, text='Letters:').grid(row=1, column=0)
-es = tkinter.Entry(m)
-es.grid(row=1, column=1, columnspan=3)
+letters = tkinter.Entry(m)
+letters.grid(row=1, column=1, columnspan=3)
 
 tkinter.Label(m, text='Start:').grid(row=3, column=0)
-est = tkinter.Entry(m)
-est.grid(row=3, column=1, columnspan=3)
+start = tkinter.Entry(m)
+start.grid(row=3, column=1, columnspan=3)
 
 tkinter.Label(m, text='Accept:').grid(row=4, column=0)
-eff = tkinter.Entry(m)
-eff.grid(row=4, column=1, columnspan=3)
+accept = tkinter.Entry(m)
+accept.grid(row=4, column=1, columnspan=3)
 
 tkinter.Label(m, text='Moves:').grid(row=5, column=0)
 moves = tkinter.Entry(m)
 moves.grid(row=5, column=1, columnspan=3)
 
 # button
-button = tkinter.Button(m, text='Submit', width=15, command=run(est,eq,moves,eff,es)).grid(row=6, column=1)
+button = tkinter.Button(m, text='Submit', width=15, command=run(start,states,moves,accept,letters).grid(row=6, column=1))
 
 #output
-outputVar = tkinter.Message(m, text = prntmvs).grid(row=7, column= 1)
+outputVar = tkinter.Message(m, text = prntNFA).grid(row=7, column= 1)
 
 # run the gui
 m.mainloop()
